82daef29e141e62f909b200b860dd0c65c391acd
roeyHonig <roeyhonig94@gmail.com>
2018-09-27 19:58:58 +0300

    created MovieListController Class

---
+++ ObjCMsapps/MovieList/MovieListViewController.h
@@ -0,0 +1,13 @@
+//
+//  MovieListViewController.h
+//  ObjCMsapps
+//
+//  Created by hackeru on 18 Tishri 5779.
+//  Copyright © 5779 student.roey.honig. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface MovieListViewController : ViewController
+
+@end
---
+++ ObjCMsapps/New Group1/MovieListViewController.m
@@ -0,0 +1,37 @@
+//
+//  MovieListViewController.m
+//  ObjCMsapps
+//
+//  Created by hackeru on 18 Tishri 5779.
+//  Copyright © 5779 student.roey.honig. All rights reserved.
+//
+
+#import "MovieListViewController.h"
+
+@interface MovieListViewController ()
+
+@end
+
+@implementation MovieListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
